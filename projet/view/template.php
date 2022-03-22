61beb2632c056375eee5685c19a3533c52a86d8f
younes khoubaz <youness.dobarim@gmail.com>
2022-03-22 23:16:43 +0100

    edit

--- projet/view/template.php
+++ projet/view/template.php
@@ -5,7 +5,6 @@
     <title><?= $titre ?></title>
 </head>
 <body>
-        <p>salaaaaaaaaaaaaaaaaaaam</p>
         <!-- hna ghaykon code dyal header fixe -->
         <header class="header">
             <section class="header-bottom">
@@ -43,50 +42,6 @@
 
 
         <footer class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-sm-12">
-                        <div class="footer-charity-text">
-                            <h2>Smia dyal Association</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
-                            <hr>
-                            <p><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></p>
-                        </div>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="row">
-                            <div class="col-md-4 col-sm-5">
-                                <div class="footer-text one">
-                                    <h3>RECENT POST</h3>
-                                    <ul>
-                                        <li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing Elit</a></li>
-                                        <li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing </a></li>
-                                        <li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing Elit</a></li>
-                                        <li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing</a></li>
-                                        <li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing Elit</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-md-4 col-sm-3">
-                                <div class="footer-text two">
-                                    <h3>LIENS</h3>
-                                    <ul>
-                                        <li><a href="#">Home</a></li>
-                                        <li><a href="#">Causes</a></li>
-                                        <li><a href="#">Event</a></li>
-                                        <li><a href="#">Blog</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-md-4 col-sm-4">
-                                <div class="footer-text one">
-                                    <h3>CONTACTEZ NOUS</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="footer_bottom">
                 <p>KHOUBAZ & ASKLOU @ 2022 | Tous droits réservés </p>
             </div>
